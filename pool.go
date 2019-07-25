f5e4445bd5c4d226c1c72060efe8b672a4f21cb9
zrufo <zrufo747@outlook.com>
2019-07-25 15:03:57 +0800

    1.1

--- pool.go
+++ pool.go
@@ -6,58 +6,76 @@ package connpool
 
 import (
 	"errors"
+	"sync"
 	"time"
 )
 
-var ErrTimeOut = errors.New("timeout")
+var (
+	ErrTimeOut     = errors.New("timeout")
+	ErrCapZero     = errors.New("cap不能小于0")
+	ErrTimeOutZero = errors.New("connTimeOut不能小于0")
+)
 
-type ConnRes interface {
+type Connection interface {
 	Close() error
+	IsOpen() bool
 }
-type Factory func() (ConnRes, error)
+
+type Factory func() (Connection, error)
 
 type Pool struct {
-	conns       chan ConnRes
+	sync.Mutex
+	numOpen     int
+	conns       chan Connection
 	factory     Factory
 	connTimeOut time.Duration
 }
 
 func NewPool(factory Factory, cap int, connTimeOut time.Duration) (*Pool, error) {
+
 	if cap <= 0 {
-		return nil, errors.New("cap不能小于0")
+		return nil, ErrCapZero
 	}
 	if connTimeOut <= 0 {
-		return nil, errors.New("connTimeOut不能小于0")
+		return nil, ErrTimeOutZero
 	}
 
 	pool := &Pool{
-		conns:       make(chan ConnRes, cap),
+		conns:       make(chan Connection, cap),
 		factory:     factory,
 		connTimeOut: connTimeOut,
 	}
 
-	for i := 0; i < cap; i++ {
-		//通过工厂方法创建连接资源
-		connRes, err := pool.new()
-		if err != nil {
-			return nil, err
-		}
-		//将连接资源插入通道中
-		pool.conns <- connRes
-	}
-
 	return pool, nil
 }
-func (p *Pool) new() (ConnRes, error) {
-	conn, err := p.factory()
-	return conn, err
+func (p *Pool) newConn() (conn Connection, err error) {
+	conn, err = p.factory()
+	return
 }
 
-func (p *Pool) Get() (conn ConnRes, err error) {
+func (p *Pool) Get() (conn Connection, err error) {
+	// 连接池未满
+	p.Lock()
+	if p.numOpen < cap(p.conns) {
+
+		conn, err = p.newConn()
+		if err != nil {
+			p.Unlock()
+			return
+		}
+
+		p.numOpen++
+		p.Unlock()
+		return
+	}
+	p.Unlock()
 
 	select {
-	case conn = <-p.conns:
+	case conn, err = <-p.conns:
 		{
+			if err != nil {
+				return nil, ErrTimeOut
+			}
 		}
 	case <-time.After(p.connTimeOut):
 		{
@@ -67,7 +85,15 @@ func (p *Pool) Get() (conn ConnRes, err error) {
 	return
 }
 
-func (p *Pool) Put(conn ConnRes) {
+func (p *Pool) Put(conn Connection) {
+	p.Lock()
+	if !conn.IsOpen() {
+		p.numOpen--
+		p.Unlock()
+		return
+	}
+	p.Unlock()
+
 	select {
 	case p.conns <- conn:
 		{
